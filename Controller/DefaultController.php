63567d0d889943b380144ef3387dabfde5335722
Jeremy Dare <jez433@gmail.com>
2013-01-28 16:50:59 -0800

    Initial Commit of Bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jez433\ClankBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return new \Symfony\Component\HttpFoundation\Response("test");
+    }
+}
---
+++ Jez433ClankBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jez433\ClankBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Jez433ClankBundle extends Bundle
+{
+}
